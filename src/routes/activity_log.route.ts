f1e68586db7332ceb5d96598cf24b776155b7b8c
kwambiee <kwambokaj2.jk@gmail.com>
2024-12-08 18:53:05 +0300

    Add JWT authentication to activity log routes

--- src/routes/activity_log.route.ts
+++ src/routes/activity_log.route.ts
@@ -5,10 +5,30 @@ import "../config/passport";
 
 const router = Router();
 
-router.post("/", createActivityLogController);
-router.get("/", getActivityLogsController);
-router.get("/:id", getActivityLogByIdController);
-router.put("/:id", updateActivityLogController);
-router.delete("/:id", deleteActivityLogController);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  createActivityLogController
+);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  getActivityLogsController
+);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  getActivityLogByIdController
+);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  updateActivityLogController
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  deleteActivityLogController
+);
 
 export default router;
